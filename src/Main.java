2ef20c6cb92f2f45fb470b380d534405dd687979
trinhdat15 <153842433+trinhdat15@users.noreply.github.com>
2024-03-14 08:01:59 +0700

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        MP3 mp3;
+        MP3 mp3 = new MP3("Chung ta cua tuong lai","Bai hat rat hay","123",50000,360);
         mp3.showInfo();
 
-        Book book;
+        Book book = new book("Truyen co tich Grimm","100 truyen","456",60000,"Grimm",599,"Truyen co tich");
         book.showInfo();
 
     }
