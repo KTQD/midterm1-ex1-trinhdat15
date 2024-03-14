de00981fe993d83c90351bc6b9f6005637ab9723
trinhdat15 <153842433+trinhdat15@users.noreply.github.com>
2024-03-14 07:41:40 +0700

    Item.java

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,18 @@
-public class Item {
+public abstract class Item {
+    public String name;
+    private String description()
+    protected String ID;
+    float price;
+    public String getDescription(){
+        return description;
+    }
+    public void setDescription(String description)
+        this.description = description;
+    public abstract void showInfo();
+    public Item(String name, String description, String ID, float price) {
+        this.name = name;
+        this.description = description;
+        this.ID = ID;
+        this.price = price;
+    }
 }
