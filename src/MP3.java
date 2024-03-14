f162b0156f8168b120fe152165fb64ce326a74e6
trinhdat15 <153842433+trinhdat15@users.noreply.github.com>
2024-03-14 07:57:00 +0700

    MP3.java

--- src/MP3.java
+++ src/MP3.java
@@ -1,15 +1,17 @@
-public abstract class MP3 extends Item {
+import  java.lang.Override;
+public class MP3 extends Item {
     private int duration;
     public MP3(String name, String description, String ID, float price, int duration) {
         super(name, description, ID, price);
         this.duration = duration;
     }
-}
-@Override
-public void showInfo(){
-    System.out.println("Ten vat pham la: "+name);
-    System.out.println("Mo ta vat pham: "+description);
-    System.out.println("ID vat pham: "+ID);
-    System.out.println("Gia vat pham la: "+price);
-    System.out.println("Tgian su dung vat pham la: "+duration);
+
+    @Override
+    public void showInfo() {
+        System.out.println("Ten vat pham la: "+name);
+        System.out.println("Mo ta vat pham: "+description);
+        System.out.println("ID vat pham: "+ID);
+        System.out.println("Gia vat pham la: "+price);
+        System.out.println("Tgian su dung vat pham la: "+duration);
+    }
 }
